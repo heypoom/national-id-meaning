f95577915713177665f081ba70f6b1f9d8de643f
Phoomparin Mano <phoomparin@gmail.com>
2019-05-30 01:33:46 +0700

    :bug: Re-arrange font priorities

--- src/app/viewer.tsx
+++ src/app/viewer.tsx
@@ -44,7 +44,7 @@ const NumberInput = styled(MaskedInput)`
 
   color: #333;
   font-weight: bold;
-  font-family: 'FuraCode Nerd Font', 'Sukhumvit Set', 'Fira Mono', 'Kanit', sans-serif;
+  font-family: 'FuraCode Nerd Font', 'Fira Mono', 'Sukhumvit Set', 'Kanit', sans-serif;
 `
 
 interface ProvinceData {
